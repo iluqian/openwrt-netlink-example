a80deeb03b22287c32efd57317697055b3ab1d1c
root <liujie@bdcom.com.cn>
2018-05-29 10:20:09 +0800

    生成一个随机号

--- signal/delay.c
+++ signal/delay.c
@@ -6,6 +6,9 @@
 #include <time.h>
 #include <sys/types.h>
 
+#define UCHAR_MAX 255
+
+/*利用select 实现简单的延时功能*/
 void rc_mdelay(int msecs)
 {
 	struct timeval tv;
@@ -14,16 +17,25 @@ void rc_mdelay(int msecs)
 	select(0, NULL, NULL, NULL, &tv);
 }
 
+/* 生成一个随机号*/
+static unsigned char rc_guess_seqnbr(void)
+{
+	srandom((unsigned int)(time(NULL)+getpid()));
+	return (unsigned char)(random() & UCHAR_MAX);
+}
+
 int main(int argc, char *argv[])
 {
 	int i;
+	unsigned char seq = 0;
 	while(1)
 	{
+		seq = rc_guess_seqnbr();
 		i++;
-		printf("%d\n",i);
-		rc_mdelay(500);
+		//printf("%d\n",i);
+		rc_mdelay(1000);
+		printf("%d\n", seq);
 	}
-
 
 	exit(EXIT_SUCCESS);
 }
